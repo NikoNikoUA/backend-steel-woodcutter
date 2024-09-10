e321dd613ccbafaea7e35e45821ddce609020c52
NikoNikoUA <128256922+NikoNikoUA@users.noreply.github.com>
2024-09-10 13:20:43 +0300

    changes in server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ import app from "./app.js";
 const port = process.env.PORT || 3000;
 
 const { DB_HOST } = process.env;
-console.log(`Connecting to DB at: ${DB_HOST}`);
 
 mongoose
   .connect(DB_HOST)
